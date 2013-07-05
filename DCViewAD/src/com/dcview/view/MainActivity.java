1829a959690b281e498307b5d2310cc878cd8628
IOKLO <leesjung@gmail.com>
2013-07-06 00:05:44 +0900

    intelliJ 프로젝트 업데이트

---
+++ DCViewAD/src/com/dcview/view/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dcview.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+// activity를 처음 실행시키면 이 화면이 뜹니다
+public class MainActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
