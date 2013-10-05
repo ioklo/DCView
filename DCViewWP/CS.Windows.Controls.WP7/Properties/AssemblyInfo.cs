0462a94725f827e05951e45154193d63d117dd42
IOKLO <leesjung@gmail.com>
2013-10-06 01:36:02 +0900

    Import WatermarkTextBox (https://github.com/JoshClose/WindowsPhoneControls/) for bug fix

---
+++ DCViewWP/CS.Windows.Controls.WP7/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿#region License
+// Copyright 2011 Josh Close
+// This file is a part of CS.Windows.Controls and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+// https://github.com/JoshClose/WindowsPhoneControls
+#endregion
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "CS.Windows.Controls" )]
+[assembly: AssemblyDescription( "Controls for Windows Phone" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "CS.Windows.Controls" )]
+[assembly: AssemblyCopyright( "Copyright © Josh Close 2011" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "2e3aa560-12ae-46fe-8914-ee066cbc4182" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion( "0.3.0.*" )]
+
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
